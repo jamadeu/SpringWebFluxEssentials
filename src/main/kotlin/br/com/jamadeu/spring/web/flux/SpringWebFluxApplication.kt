83ab7699a676e02edaa9de46167f1f149227ccc0
Jean Amadeu <jean.amadeu2@gmail.com>
2022-05-12 06:41:19 -0300

    setup project

---
+++ src/main/kotlin/br/com/jamadeu/spring/web/flux/SpringWebFluxApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jamadeu.spring.web.flux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebFluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebFluxApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jamadeu/spring/web/flux/SpringWebFluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jamadeu.spring.web.flux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
