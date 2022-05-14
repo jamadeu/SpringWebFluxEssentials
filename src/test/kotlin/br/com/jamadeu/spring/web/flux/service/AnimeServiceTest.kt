e1f29a4ad2289ee64ce855b929858f09561aefee
Jean Amadeu <jean.amadeu2@gmail.com>
2022-05-14 06:26:27 -0300

    service unit tests

--- src/test/kotlin/br/com/jamadeu/spring/web/flux/service/AnimeServiceTest.kt
+++ src/test/kotlin/br/com/jamadeu/spring/web/flux/service/AnimeServiceTest.kt
@@ -4,8 +4,8 @@ import br.com.jamadeu.spring.web.flux.model.Anime
 import br.com.jamadeu.spring.web.flux.repository.AnimeRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.BDDMockito.anyLong
-import org.mockito.BDDMockito.`when`
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.*
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -19,13 +19,13 @@ import kotlin.random.Random
 internal class AnimeServiceTest {
 
     @Mock
-    lateinit var animeRepository:AnimeRepository
+    lateinit var animeRepository: AnimeRepository
 
     @InjectMocks
     lateinit var animeService: AnimeService
 
     @Test
-    fun `findAll returns a flux of anime when successful`(){
+    fun `findAll returns a flux of anime when successful`() {
         `when`(animeRepository.findAll()).thenReturn(Flux.just(anime()))
 
         StepVerifier.create(animeService.findAll())
@@ -35,7 +35,7 @@ internal class AnimeServiceTest {
     }
 
     @Test
-    fun `findById returns a mono with anime when it exists`(){
+    fun `findById returns a mono with anime when it exists`() {
         val anime = anime()
         `when`(animeRepository.findById(anime.id)).thenReturn(Mono.just(anime))
 
@@ -46,7 +46,7 @@ internal class AnimeServiceTest {
     }
 
     @Test
-    fun `findById returns a mono error when anime does not exists`(){
+    fun `findById returns a mono error when anime does not exists`() {
         `when`(animeRepository.findById(anyLong())).thenReturn(Mono.empty())
 
         StepVerifier.create(animeService.findById(Random.nextLong()))
@@ -55,7 +55,48 @@ internal class AnimeServiceTest {
             .verify()
     }
 
+    @Test
+    fun `save creates an anime when successful`() {
+        val anime = anime()
+        `when`(animeRepository.save(anime)).thenReturn(Mono.just(anime))
 
+        StepVerifier.create(animeService.save(anime))
+            .expectSubscription()
+            .expectNext(anime)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `delete removes the anime when successful`() {
+        val anime = anime()
+        `when`(animeRepository.deleteById(anime.id)).thenReturn(Mono.empty())
+
+        StepVerifier.create(animeService.delete(anime.id))
+            .expectSubscription()
+            .verifyComplete()
+    }
+
+    @Test
+    fun `update save updated anime and returns empty Mono when successful`() {
+        val anime = anime()
+        `when`(animeRepository.save(anime)).thenReturn(Mono.empty())
+        `when`(animeRepository.findById(anime.id)).thenReturn(Mono.just(anime))
+
+        StepVerifier.create(animeService.update(anime))
+            .expectSubscription()
+            .verifyComplete()
+    }
+
+    @Test
+    fun `update returns Mono error when anime does not exist`() {
+        val anime = anime()
+        `when`(animeRepository.findById(anime.id)).thenReturn(Mono.empty())
+
+        StepVerifier.create(animeService.update(anime))
+            .expectSubscription()
+            .expectError(ResponseStatusException::class.java)
+            .verify()
+    }
 
     private fun anime(
         id: Long = 1L,
